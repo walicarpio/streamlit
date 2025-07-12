aaf136a5af79139f1c430dd34e96f65f21eb95e9
Eduardo Berrios <111591642+walicarpio@users.noreply.github.com>
2025-07-11 22:47:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-pip install --upgrade kaleido
 from io import BytesIO
 
 st.set_page_config(page_title="Dashboard SAVAL", layout="wide")
